043a4f0332e9f595e9bd43e1a469edc1e87aa075
enji-coder <kailash.solanki@tops-int.com>
2025-01-04 12:51:30 +0530

    updates

---
+++ Module-3/3.4 Recursion Function/example1.py
@@ -0,0 +1,10 @@
+def factorial(n):
+    # Base case
+    if n == 0 or n == 1:
+        return 1
+    # Recursive case
+    else:
+        return n * factorial(n - 1)
+
+# Testing the function
+print(factorial(5))  # Output: 120
